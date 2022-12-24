8301bd43377d2fcddf68ee3e85acdaa131411a41
Ahmad Yasser <ahmadyas_ser@icloud.com>
2022-12-24 17:58:54 +0200

    Add docs to created classes

---
+++ Rick&Morty/APIClient/RMEndpoint.swift
@@ -0,0 +1,15 @@
+//
+//  RMEndpoint.swift
+//  Rick&Morty
+//
+//  Created by Ahmad Yasser on 24/12/2022.
+//
+
+import Foundation
+
+/// Represents unique API endpoint
+@frozen enum RMEndpoint: String {
+    case character
+    case location
+    case episode
+}
---
+++ Rick&Morty/APIClient/RMRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RMRequest.swift
+//  Rick&Morty
+//
+//  Created by Ahmad Yasser on 24/12/2022.
+//
+
+import Foundation
+
+/// Object that represents a single API call
+final class RMRequest {
+    
+}
